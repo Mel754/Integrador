e6c5205f2db4dca7d788fd61abb0bdecde386bf9
Emadiaz04 <142610024+Emadiaz04@users.noreply.github.com>
2025-10-19 18:35:10 -0500

    Add route to distribute acta

--- routes/actas.js
+++ routes/actas.js
@@ -4,5 +4,6 @@ const A = require('../controllers/actasController');
 
 router.post('/:meeting_id/html', verificarToken, A.renderActaHtml);
 router.post('/:meeting_id/pdf', verificarToken, A.renderActaPdf);
+router.post('/:meeting_id/distribuir', verificarToken, A.distribuirActa);
 
 module.exports = router;
