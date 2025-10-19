5e590701f8b8d18075143d3465423b61e65f76ff
Emadiaz04 <142610024+Emadiaz04@users.noreply.github.com>
2025-10-19 18:34:29 -0500

    Add route to generate PDF for actas

--- routes/actas.js
+++ routes/actas.js
@@ -3,5 +3,6 @@ const { verificarToken } = require('../auth');
 const A = require('../controllers/actasController');
 
 router.post('/:meeting_id/html', verificarToken, A.renderActaHtml);
+router.post('/:meeting_id/pdf', verificarToken, A.renderActaPdf);
 
 module.exports = router;
