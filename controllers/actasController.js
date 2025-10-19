3b43e66ed0ff7676e579b0b2e52ccb82515a8091
Emadiaz04 <142610024+Emadiaz04@users.noreply.github.com>
2025-10-19 18:44:18 -0500

    Remove metrics function from actasController
    Removed metrics function and related database queries from actasController.js.

--- controllers/actasController.js
+++ controllers/actasController.js
@@ -55,22 +55,6 @@ exports.distribuirActa = async (req, res, next) => {
     res.json({ ok: true, enviados: participantes.length });
   } catch (e) { next(e); }
 };
-
-const pool = require('../db');
-
-exports.metrics = async (req, res, next) => {
-  try {
-    const [[m1]] = await pool.query('SELECT COUNT(*) AS reuniones FROM meetings');
-    const [[m2]] = await pool.query("SELECT COUNT(*) AS actas FROM tareas WHERE estado='hecha'"); // ejemplo
-    const [[m3]] = await pool.query("SELECT SUM(estado='pendiente') as pendientes, SUM(estado='hecha') as hechas FROM tareas");
-    res.json({
-      reuniones: m1.reuniones,
-      actas: m2.actas,
-      tareas: { pendientes: m3.pendientes || 0, hechas: m3.hechas || 0 }
-    });
-  } catch (e) { next(e); }
-};
-
   
 for (const a of acta.acuerdos) {
   await pool.query(
