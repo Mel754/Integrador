6d728de320fbbf876315ab42b98bf105c4af6165
Emadiaz04 <142610024+Emadiaz04@users.noreply.github.com>
2025-10-19 18:43:41 -0500

    Add metrics endpoint to actasController

--- controllers/actasController.js
+++ controllers/actasController.js
@@ -56,6 +56,21 @@ exports.distribuirActa = async (req, res, next) => {
   } catch (e) { next(e); }
 };
 
+const pool = require('../db');
+
+exports.metrics = async (req, res, next) => {
+  try {
+    const [[m1]] = await pool.query('SELECT COUNT(*) AS reuniones FROM meetings');
+    const [[m2]] = await pool.query("SELECT COUNT(*) AS actas FROM tareas WHERE estado='hecha'"); // ejemplo
+    const [[m3]] = await pool.query("SELECT SUM(estado='pendiente') as pendientes, SUM(estado='hecha') as hechas FROM tareas");
+    res.json({
+      reuniones: m1.reuniones,
+      actas: m2.actas,
+      tareas: { pendientes: m3.pendientes || 0, hechas: m3.hechas || 0 }
+    });
+  } catch (e) { next(e); }
+};
+
   
 for (const a of acta.acuerdos) {
   await pool.query(
