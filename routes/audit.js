4db909e3c036dbcfb9ca0f21e892e61317278970
Mel <142316659+Mel754@users.noreply.github.com>
2025-10-21 14:34:53 -0500

    actualizado

--- routes/audit.js
+++ routes/audit.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const auditController = require('../controllers/auditController');
+const auditController = require('./controllers/auditController');
 
 router.get('/logs', auditController.getLogs);
 
 router.get('/stats', auditController.getAuditStats);
 
 router.get('/export', auditController.exportLogs);
+
 
 module.exports = router;
