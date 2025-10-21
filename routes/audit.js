c4fe677f0fdd027e8a263188d8c7a3b41bed0afb
Mel <142316659+Mel754@users.noreply.github.com>
2025-10-21 13:50:33 -0500

    Add files via upload

---
+++ routes/audit.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const auditController = require('../controllers/auditController');
+
+router.get('/logs', auditController.getLogs);
+
+router.get('/stats', auditController.getAuditStats);
+
+router.get('/export', auditController.exportLogs);
+
+module.exports = router;
